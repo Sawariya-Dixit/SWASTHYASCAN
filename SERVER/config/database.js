cdc2b3b77bb70b349506949e88af27f990c41471
Sawariya Dixit <sawariyadixit085@gmail.com>
2026-09-17 12:30:12 +0530

    database connect

--- SERVER/config/database.js
+++ SERVER/config/database.js
@@ -1,14 +1,17 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
+const dns = require("dns");
 
-async function connectDB() {
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected successfully");
-  } catch (err) {
-    console.error("MongoDB connection failed:", err.message);
+    await mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected successfully"))
+    .catch((err) => console.error("MongoDB connection error:", err));
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
     process.exit(1);
-  }
-}
-
+  } 
+};
 module.exports = connectDB;
